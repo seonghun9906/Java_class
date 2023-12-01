cdbd9395abfc07c886b24ddb6dffffbaa906f5dc
seonghun9906 <jsh990616@naver.com>
2023-12-01 16:22:12 +0900

    학점계산

---
+++ src/ch04_if/ex03_ifExampleScore.java
@@ -0,0 +1,27 @@
+package ch04_if;
+import java.util.Scanner;
+public class ex03_ifExampleScore {
+
+	public static void main(String[] args) {
+		// 스캐너로 점수 (score)를 하나 입력받아서
+//		90점 이상 A
+//		80점 이상 B
+//		...
+//		60점 보다 낮으면 F 출력하기
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("점수를 입력하세요 : ");
+		int score = scanner.nextInt();
+		
+		if(score >=90){
+			System.out.println("A");
+		}else if(score >=80){
+			System.out.println("B");
+		}else if(score >=70) {
+			System.out.println("C");
+		}else if(score >=60) {
+			System.out.println("D");
+		}else {
+				System.out.println("F");
+		}
+	}
+}
