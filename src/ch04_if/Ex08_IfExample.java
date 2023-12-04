2dc6ed416e7bf414337f91bf1725fdedb65ebeef
seonghun9906 <jsh990616@naver.com>
2023-12-04 11:21:55 +0900

    if exmaple

--- src/ch04_if/Ex08_IfExample.java
+++ src/ch04_if/Ex08_IfExample.java
@@ -40,3 +40,20 @@ public class Ex08_IfExample {
 	}
 
 }
+
+
+//		int max =0;
+//		if(num1 > num2) {
+//			if(num1>num3) {
+//				max = num1;
+//			}else {
+//				max = num3;
+//			}
+//		}else {
+//			if(num2>num3)	{
+//				max = num2;
+//			}else {
+//				max=num3;
+//			}
+//		}
+//		System.out.println("가장 큰 수는 " + max + "입니다.");
