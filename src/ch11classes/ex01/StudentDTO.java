071bbe1f47bbb358a78e50d72e9250f1b71cb0b4
seonghun9906 <jsh990616@naver.com>
2023-12-14 15:31:14 +0900

    20231214 - 클래스 분리 예제

---
+++ src/ch11classes/ex01/StudentDTO.java
@@ -0,0 +1,76 @@
+package ch11classes.ex01;
+
+public class StudentDTO {
+    private Long id;
+    private String StudentName;
+    private String StudentNumber;
+    private String StudentMajor;
+    private String StudentMobile;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStudentName() {
+        return StudentName;
+    }
+
+    public void setStudentName(String studentName) {
+        StudentName = studentName;
+    }
+
+    public String getStudentNumber() {
+        return StudentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        StudentNumber = studentNumber;
+    }
+
+    public String getStudentMajor() {
+        return StudentMajor;
+    }
+
+    public void setStudentMajor(String studentMajor) {
+        StudentMajor = studentMajor;
+    }
+
+    public String getStudentMobile() {
+        return StudentMobile;
+    }
+
+    public void setStudentMobile(String studentMobile) {
+        StudentMobile = studentMobile;
+    }
+
+    public StudentDTO(){
+
+    }
+    private static Long idValue = 1L;
+
+    public StudentDTO(String studentName, String studentNumber, String studentMajor, String studentMobile) {
+        this.id = idValue++;
+        StudentName = studentName;
+        StudentNumber = studentNumber;
+        StudentMajor = studentMajor;
+        StudentMobile = studentMobile;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", StudentName='" + StudentName + '\'' +
+                ", StudentNumber='" + StudentNumber + '\'' +
+                ", StudentMajor='" + StudentMajor + '\'' +
+                ", StudentMobile='" + StudentMobile + '\'' +
+                '}';
+    }
+}
+
+
---
+++ src/ch11classes/ex01/StudentService.java
@@ -0,0 +1,4 @@
+package ch11classes.ex01;
+
+public class StudentService {
+}
