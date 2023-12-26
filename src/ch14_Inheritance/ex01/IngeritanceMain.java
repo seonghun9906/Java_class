b5f86636041f17621c0dc948212da41b1bee5c68
seonghun9906 <jsh990616@naver.com>
2023-12-27 08:24:52 +0900

    Hash map (사용방법) / 상속 (Ingeritance)

--- src/ch14_Inheritance/ex01/IngeritanceMain.java
+++ src/ch14_Inheritance/ex01/IngeritanceMain.java
@@ -2,20 +2,20 @@ package ch14_Inheritance.ex01;
 
 public class IngeritanceMain {
     public static void main(String[] args) {
-//        ParentClass parentClass = new ParentClass();
+        ParentClass parentClass = new ParentClass();
 
 
-//        parentClass.setName("이름1");
-//        parentClass.setAge(10);
-//        System.out.println("parentClass = " + parentClass);
-//        parentClass.hello();
+        parentClass.setName("이름1");
+        parentClass.setAge(10);
+        System.out.println("parentClass = " + parentClass);
+        parentClass.hello();
 
-//        ChildClass childClass = new ChildClass();
-//        //상속을 받았기 떄문에 자식클래스에서 get, set 메서듯 사용 가능.
-//        childClass.setName("자식");
-//        childClass.setAge(100);
-//        System.out.println("childClass = " + childClass);
-//        childClass.hello();
+        ChildClass childClass = new ChildClass();
+        //상속을 받았기 떄문에 자식클래스에서 get, set 메서듯 사용 가능.
+        childClass.setName("자식");
+        childClass.setAge(100);
+        System.out.println("childClass = " + childClass);
+        childClass.hello();
 //
         //좌변 : 부모타임의 객체, 우변 : 자식클래스 생성자
         ParentClass parentClass1 = new ChildClass();
